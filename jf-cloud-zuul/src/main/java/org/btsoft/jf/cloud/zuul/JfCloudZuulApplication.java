5c0db646a5a3b770456778b98ff926c68af63928
bchen1024 <bchen_1024@126.com>
2018-03-21 22:54:08 +0800

    解决跨域问题

--- jf-cloud-zuul/src/main/java/org/btsoft/jf/cloud/zuul/JfCloudZuulApplication.java
+++ jf-cloud-zuul/src/main/java/org/btsoft/jf/cloud/zuul/JfCloudZuulApplication.java
@@ -1,8 +1,15 @@
 package org.btsoft.jf.cloud.zuul;
 
+import org.btsoft.jf.cloud.zuul.filter.AuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +17,24 @@ public class JfCloudZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JfCloudZuulApplication.class, args);
+	}
+
+	@Bean
+	public AuthorizationFilter authorizationFilter(){
+		return new AuthorizationFilter();
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin(CorsConfiguration.ALL);
+		config.addAllowedHeader(CorsConfiguration.ALL);
+		config.addAllowedMethod(CorsConfiguration.ALL);
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
 	}
 }
---
+++ jf-cloud-zuul/src/main/java/org/btsoft/jf/cloud/zuul/filter/AuthorizationFilter.java
@@ -0,0 +1,36 @@
+package org.btsoft.jf.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthorizationFilter extends ZuulFilter {
+
+    private final static Logger logger= LoggerFactory.getLogger(AuthorizationFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.debug(String.format("[%s] %s",request.getMethod(),request.getRequestURL().toString()));
+        return null;
+    }
+}
