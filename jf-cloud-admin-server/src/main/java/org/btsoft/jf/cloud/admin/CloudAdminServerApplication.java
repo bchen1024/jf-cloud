b0346edf099b09255137d81214f6b5be90d6d40f
bchen1024 <bchen_1024@126.com>
2019-04-15 23:19:04 +0800

    pom增加监控点组件

--- jf-cloud-admin-server/src/main/java/org/btsoft/jf/cloud/admin/CloudAdminServerApplication.java
+++ jf-cloud-admin-server/src/main/java/org/btsoft/jf/cloud/admin/CloudAdminServerApplication.java
@@ -1,16 +1,12 @@
 package org.btsoft.jf.cloud.admin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
 public class CloudAdminServerApplication {
