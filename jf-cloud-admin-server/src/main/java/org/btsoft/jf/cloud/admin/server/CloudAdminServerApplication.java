f35b2c153ccf3d05a8a0e555f84daec1cc1d6cf3
bchen1024 <bchen_1024@126.com>
2019-04-28 16:37:40 +0800

    提取公共默认属性单独维护

--- jf-cloud-admin-server/src/main/java/org/btsoft/jf/cloud/admin/server/CloudAdminServerApplication.java
+++ jf-cloud-admin-server/src/main/java/org/btsoft/jf/cloud/admin/server/CloudAdminServerApplication.java
@@ -1,4 +1,4 @@
-package org.btsoft.jf.cloud.admin;
+package org.btsoft.jf.cloud.admin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
